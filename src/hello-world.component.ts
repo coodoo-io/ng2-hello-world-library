8ccc173122cc157de571b2ff32d5548de87f86f7
Markus Kühle <markus.kuehle@coodoo.de>
2017-01-04 13:35:10 +0100

    cleanup

--- src/hello-world.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'hello-world-component',
-  template: `<h1>Hello World coodoo library Version 2</h1>`
-})
-export class HelloWorldComponent {
-  constructor() {}
-}

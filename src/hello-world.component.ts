acb9b4f0c21d278c1037cdab72fb6329882a2512
Markus Kühle <markus.kuehle@coodoo.de>
2017-01-04 10:25:10 +0100

    Text changed

--- src/hello-world.component.ts
+++ src/hello-world.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'hello-world-component',
-  template: `<h1>Hello World coodoo library</h1>`
+  template: `<h1>Hello World coodoo library Version 2</h1>`
 })
 export class HelloWorldComponent {
   constructor() {}

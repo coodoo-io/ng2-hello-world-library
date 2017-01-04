34c344e82527de2046c146d0632d872647de289f
Markus Kühle <markus.kuehle@coodoo.de>
2017-01-04 17:31:35 +0100

    Dist folder

---
+++ dist/hello-world.component.js
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+export var HelloWorldComponent = (function () {
+    function HelloWorldComponent() {
+    }
+    HelloWorldComponent.decorators = [
+        { type: Component, args: [{ selector: 'hello-world-component', templateUrl: './hello-world.component.html' },] },
+    ];
+    /** @nocollapse */
+    HelloWorldComponent.ctorParameters = function () { return []; };
+    return HelloWorldComponent;
+}());
+//# sourceMappingURL=hello-world.component.js.map
